fb10ddb55f369e1631e23f4e8faf5028fca6efc5
Milad-Gemini <milad.mohammadi.s74@gmail.com>
2025-12-24 12:26:42 +0330

    initial commit

--- javascript.js
+++ javascript.js
@@ -430,3 +430,14 @@ const calcAge2 = function (birthYear) {
 // Arrow function
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntillRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //return retirement;
+  return`${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntillRetirement(1991, 'jonas'));
+console.log(yearsUntillRetirement(1980, 'milad'));
+
