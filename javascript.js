3cf0cfef5cc97eec17afed66a0f05acd45573b31
Milad-Gemini <milad.mohammadi.s74@gmail.com>
2025-12-25 13:53:00 +0330

    initial commit

--- javascript.js
+++ javascript.js
@@ -501,6 +501,34 @@ console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);*/
 
 //video 44
+const friends = ['Michael' , 'steven' , 'peter'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'jonas';
+const jonas = [firstName, 'moha', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018]
+
+
+
+
 
 
 
